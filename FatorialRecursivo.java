42f9c5301af647c812502a1de09d9e7ac23d92ac
Joilton Cabral Araujo <joiltoncabral@gmail.com>
2021-06-03 21:12:54 -0400

    Fatorial recursivo ímplementado

--- FatorialRecursivo.java
+++ FatorialRecursivo.java
@@ -1,3 +1,12 @@
 class FatorialRecursiva{
-    
+    public static int calcularFatorialRecursividade(int n) throws IllegalAccessException{
+
+        if (n == 0)
+        return 1;
+        return n * calcularFatorialRecursividade(n-1);
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(calcularFatorialRecursividade(5));
+    }
 }
